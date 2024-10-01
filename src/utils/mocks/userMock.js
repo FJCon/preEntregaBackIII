e31e3203180154b01e7e5bba993503f580de7d94
unknown <conilfernando@gmail.com>
2024-10-01 11:17:00 -0300

    mocking pet & user

--- src/utils/mocks/userMock.js
+++ src/utils/mocks/userMock.js
@@ -1,14 +1,21 @@
-function generateUsers(numUsers) {
-  const users = [];
-  for (let i = 0; i < numUsers; i++) {
-    users.push({
-      id: i + 1,
-      name: `Usuario ${i + 1}`,
-      email: `usuario${i + 1}@example.com`,
-      // ... otros datos
-    });
-  }
-  return users;
-}
+import bcrypt from 'bcrypt';
+import { faker } from '@faker-js/faker';
 
-module.exports = { generateUsers };
+export const generateUsers = (numUsers) => {
+    const users = [];
+
+    for (let i = 0; i < numUsers; i++) {
+        const user = {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: bcrypt.hashSync('coder123', 10), 
+            role: faker.helpers.arrayElement(['user', 'admin']), 
+            pets: [],
+        };
+
+        users.push(user);
+    }
+
+    return users;
+};
